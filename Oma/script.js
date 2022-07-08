d24a951bab9e4770e419e4b2ffeea8b50b6c5ab5
Gold <davey3680@gmail.com>
2022-07-08 18:30:53 +0200

    final edit

--- Oma/script.js
+++ Oma/script.js
@@ -1,7 +1,6 @@
 
 //load more
 const loadMore=document.querySelector('.more')
-
 const moreItems=document.querySelectorAll('template')
 let i=0
 loadMore.addEventListener('click',()=>{
@@ -9,18 +8,4 @@ loadMore.addEventListener('click',()=>{
     i++
 })
 
-//theme
-const htmlEl = document.getElementsByTagName('html')[0];
-
-const toggleTheme = (theme) => {
-    htmlEl.dataset.theme = theme;
-}
-
-
-const card=document.querySelector('.card')
-const main=document.querySelector('main')
-
-// card.addEventListener('click',(e)=>{
-//     card.classList.toggle('active')     
-// })
 
