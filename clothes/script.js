d0623cf128ea627d34faaa230ceeec599bed1654
gold3680 <davey3680@gmail.com>
2022-07-13 03:09:41 +0100

    easy peezy

---
+++ clothes/script.js
@@ -0,0 +1,8 @@
+//load more
+const loadMore=document.querySelector('.more')
+const moreItems=document.querySelectorAll('template')
+let i=0
+loadMore.addEventListener('click',()=>{
+    moreItems[i].outerHTML=moreItems[i].innerHTML
+    i++
+})
